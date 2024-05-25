e4617eba610f96c7e7c9fed173d4380ca619b0fa
MuhammadYusuf07 <yusuftkr077@gmail.com>
2024-05-25 15:14:47 +0700

    tic-tac-toe menit 30:05

--- tic-tac-toe/vite-project/src/App.jsx
+++ tic-tac-toe/vite-project/src/App.jsx
@@ -1,31 +1,35 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState("");
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [square, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+
+    nextSquares[i] = "X";
+
+    setSquares(nextSquares);
+  }
+
   return (
     <div className="board">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={square[1]} />
+      <Square value={square[2]} />
+      <Square value={square[3]} />
+      <Square value={square[4]} />
+      <Square value={square[5]} />
+      <Square value={square[6]} />
+      <Square value={square[7]} />
+      <Square value={square[8]} />
     </div>
   );
 }
